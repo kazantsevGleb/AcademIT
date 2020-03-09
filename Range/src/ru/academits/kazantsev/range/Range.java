6ebefab06255c3879bd692e1f06ea435011532f3
glebkazancev <gleb3gs@gmail.com>
2020-03-09 21:49:11 +0700

    Range* task

--- Range/src/ru/academits/kazantsev/range/Range.java
+++ Range/src/ru/academits/kazantsev/range/Range.java
@@ -16,4 +16,67 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    @Override
+    public String toString() {
+        return String.format("%.1f, %.1f", this.from, this.to);
+    }
+
+    public Range getIntersectionRanges(Range range) {
+        if (this.from >= range.to || this.to <= range.from) {
+            return null;
+        } else {
+            if (this.from > range.from && this.to > range.to) {
+                return new Range(this.from, range.to);
+            } else if (range.from > this.from && this.to > range.to) {
+                return new Range(range.from, range.to);
+            } else if (range.from < this.from && this.to < range.to) {
+                return new Range(this.from, this.to);
+            } else {
+                return new Range(range.from, this.to);
+            }
+        }
+    }
+
+    public Range[] getAdditionRanges(Range range) {
+        Range[] additionRange = new Range[2];
+
+        if (this.from > range.to) {
+            additionRange[0] = new Range(range.from, range.to);
+            additionRange[1] = new Range(this.from, this.to);
+            return additionRange;
+        } else if (this.to < range.from) {
+            additionRange[0] = new Range(this.from, this.to);
+            additionRange[1] = new Range(range.from, range.to);
+            return additionRange;
+        } else {
+            additionRange = new Range[1];
+            additionRange[0] = new Range(Math.min(this.from, range.from), Math.max(this.to, range.to));
+            return additionRange;
+        }
+    }
+
+    public Range[] getSubtractionRanges(Range range) {
+        Range[] subtractionRange = new Range[1];
+
+        if (this.from >= range.to || this.to <= range.from) {
+            subtractionRange[0] = new Range(this.from, this.to);
+            return subtractionRange;
+        } else {
+            if (range.from < this.from && range.to <= this.to) {
+                subtractionRange[0] = new Range(range.from, this.from);
+                return subtractionRange;
+            } else if (this.from <= range.from && this.to < range.to) {
+                subtractionRange[0] = new Range(this.to, range.to);
+                return subtractionRange;
+            } else if (this.from < range.from && range.to < this.to) {
+                subtractionRange = new Range[2];
+                subtractionRange[0] = new Range(this.from, range.from);
+                subtractionRange[1] = new Range(range.to, this.to);
+                return subtractionRange;
+            } else {
+                return new Range[0];
+            }
+        }
+    }
 }
--- Range/src/ru/academits/kazantsev/range_main/Main.java
+++ Range/src/ru/academits/kazantsev/range_main/Main.java
@@ -2,12 +2,19 @@ package ru.academits.kazantsev.range_main;
 
 import ru.academits.kazantsev.range.Range;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(4.1, 8.2);
+        Range range = new Range(1, 19);
+        Range range1 = new Range(5, 15);
 
-        System.out.println(range.isInside(3));
         System.out.println(range.isInside(6));
-        System.out.println(range.getLength());
+
+        System.out.println(range1.getLength());
+
+        System.out.println(range.getIntersectionRanges(range1));
+        System.out.println(Arrays.toString(range.getAdditionRanges(range1)));
+        System.out.println(Arrays.toString(range.getSubtractionRanges(range1)));
     }
 }
