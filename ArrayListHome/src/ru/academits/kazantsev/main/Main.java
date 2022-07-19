1457cd5d78b4a9bb1ea1c47ec623d33e42296d62
kazantsevGleb <gleb3gs@gmail.com>
2022-07-19 13:13:18 +0700

    fix ArrayListHome

--- ArrayListHome/src/ru/academits/kazantsev/main/Main.java
+++ ArrayListHome/src/ru/academits/kazantsev/main/Main.java
@@ -1,47 +1,46 @@
 package ru.academits.kazantsev.main;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        FileReader fileReader = new FileReader("textFile");
-        Scanner scanner = new Scanner(fileReader);
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("textFile"))) {
+            ArrayList<String> lines = new ArrayList<>();
 
-        ArrayList<String> strings = new ArrayList<>();
+            while (reader.ready()) {
+                lines.add(reader.readLine());
+            }
 
-        while (scanner.hasNextLine()) {
-            String p = scanner.nextLine();
-
-            strings.add(String.valueOf(p));
+            System.out.println("Список с текстом - " + lines);
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла, убедитесь в его наличии!");
         }
 
-        System.out.println(strings);
+        ArrayList<Integer> numbers1 = new ArrayList<>(
+                Arrays.asList(1, 2, 2, 4, 2, 6, 2, 8, 9, 10, 10, 12, 100, 200, 500, 333));
 
-        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 2, 4, 2, 6, 2, 8, 9, 10, 10, 12));
-        Iterator<Integer> numberIterator = numbers.iterator();
-
-        while (numberIterator.hasNext()) {
-            if (numberIterator.next() % 2 == 0) {
-                numberIterator.remove();
+        for (int i = 0; i < numbers1.size(); i++) {
+            if (numbers1.get(i) % 2 == 0) {
+                numbers1.remove(i);
+                i--;
             }
         }
 
-        System.out.println(numbers);
+        System.out.println("Список нечетных чисел - " + numbers1);
 
-        numbers = new ArrayList<>(Arrays.asList(2, 2, 4, 1, 5, 9, 9, 9, 10));
-        ArrayList<Integer> numbersWithoutRepeat = new ArrayList<>();
+        ArrayList<Integer> numbers2 = new ArrayList<>(
+                Arrays.asList(2, 2, 4, 1, 5, 9, 9, 9, 10));
 
-        for (Integer number : numbers) {
+        ArrayList<Integer> numbersWithoutRepeat = new ArrayList<>(numbers2.size());
+
+        for (int number : numbers2) {
             if (!numbersWithoutRepeat.contains(number)) {
                 numbersWithoutRepeat.add(number);
             }
         }
 
-        System.out.println(numbersWithoutRepeat);
+        System.out.println("Список неповторяющихся чисел - " + numbersWithoutRepeat);
     }
 }
