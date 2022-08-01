c7eacfa19e66233edf36232ede5041fed8b47989
kazantsevGleb <gleb3gs@gmail.com>
2022-08-02 00:59:11 +0700

    Частичное выполнение задачи, на данный момент, метод вычисления определителя матрицы не реализован.

--- Matrix/src/ru/academits/kazantsev/matrix/Matrix.java
+++ Matrix/src/ru/academits/kazantsev/matrix/Matrix.java
@@ -7,30 +7,30 @@ import java.util.Arrays;
 public class Matrix {
     private final Vector[] components;
 
-    public Matrix(int n, int m) {
-        if (n <= 0) {
+    public Matrix(int lineCount, int columnCount) {
+        if (lineCount <= 0) {
             throw new IllegalArgumentException(
-                    String.format("Ошибка создания матрицы с количеством строк = %d, количество строк в матрице должно быть больше 0", n)
+                    String.format("Ошибка создания матрицы с количеством строк = %d, количество строк в матрице должно быть больше 0", lineCount)
             );
         }
 
-        if (m <= 0) {
+        if (columnCount <= 0) {
             throw new IllegalArgumentException(
-                    String.format("Ошибка создания матрицы с количеством столбцов = %d, количество столбцов в матрице должно быть больше 0", m)
+                    String.format("Ошибка создания матрицы с количеством столбцов = %d, количество столбцов в матрице должно быть больше 0", columnCount)
             );
         }
 
-        Vector[] vectors = new Vector[n];
+        Vector[] vectors = new Vector[lineCount];
 
-        for (int i = 0; i < n; i++) {
-            vectors[i] = new Vector(m);
+        for (int i = 0; i < lineCount; i++) {
+            vectors[i] = new Vector(columnCount);
         }
 
         this.components = Arrays.copyOf(vectors, vectors.length);
     }
 
     public Matrix(Matrix matrix) {
-        components = matrix.components;
+        components = Arrays.copyOf(matrix.components, matrix.components.length);
     }
 
     public Matrix(double[][] matrix) {
@@ -43,7 +43,7 @@ public class Matrix {
             }
         }
 
-        for (int i = 0; i < matrix.length; i++){
+        for (int i = 0; i < matrix.length; i++) {
             components[i] = new Vector(vectorLength, matrix[i]);
         }
     }
@@ -59,6 +59,33 @@ public class Matrix {
         for (int i = 0; i < vectors.length; i++) {
             components[i] = new Vector(vectorLength, vectors[i]);
         }
+    }
+
+    public int getLineCount() {
+        return components.length;
+    }
+
+    public int getColumnCount() {
+        return components[0].getSize();
+    }
+
+    public Vector getLineVector(int index) {
+        if (index < 0 || index >= components.length) {
+            throw new IndexOutOfBoundsException(
+                    String.format("Необходимо ввести значение индекса в диапазоне от 0 до %d, передано %d", components.length - 1, index));
+        }
+
+        return components[index];
+    }
+
+    public Vector getColumnVector(int index) {
+        Vector columnVector = new Vector(components.length);
+
+        for (int i = 0; i < components.length; i++) {
+            columnVector.setComponent(i, components[i].getComponent(index));
+        }
+
+        return columnVector;
     }
 
     public Vector getComponent(int index) {
@@ -79,16 +106,129 @@ public class Matrix {
         components[index] = vector;
     }
 
+    public Matrix transpose() {
+        Matrix transposedMatrix = new Matrix(getColumnCount(), getLineCount());
+        for (int i = 0; i < transposedMatrix.components.length; i++) {
+            transposedMatrix.components[i] = getColumnVector(i);
+        }
+
+        return transposedMatrix;
+    }
+
+    public Matrix getProduct(double scalar) {
+        for (Vector component : components) {
+            component.multiply(scalar);
+        }
+
+        return new Matrix(components);
+    }
+
+    public double getDeterminant(Matrix matrix) {
+
+        return 0;
+    }
+
+    public Matrix multiplyByVector(Vector vector) {
+        int vectorSize = vector.getSize();
+        int matrixColumnCount = getColumnCount();
+        Vector columnVector = getColumnVector(0);
+
+        if (matrixColumnCount != 1) {
+            throw new ArithmeticException(String.format("Произведение не определено, количество столбцов матрицы (передано %d) должно быть равно размерности вектора: 1",
+                    matrixColumnCount));
+        }
+
+        Matrix resultMatrix = new Matrix(getLineCount(), vectorSize);
+
+        for (int i = 0; i < resultMatrix.components.length; i++) {
+            for (int j = 0; j < vectorSize; j++) {
+                resultMatrix.components[i].setComponent(j, columnVector.getComponent(i) * vector.getComponent(j));
+            }
+        }
+
+        return resultMatrix;
+    }
+
+    public Matrix getSum(Matrix matrix) {
+        if (getColumnCount() != matrix.getColumnCount() || getLineCount() != matrix.getLineCount()) {
+            throw new ArithmeticException(String.format("Сумма не определена, размерность матриц должна быть равна, " +
+                    "Матрица 1 - %d x %d, Матрица 2 - %d x %d", getLineCount(), getColumnCount(), matrix.getLineCount(), matrix.getColumnCount()));
+        }
+
+        for (int i = 0; i < components.length; i++) {
+            components[i].add(matrix.components[i]);
+        }
+
+        return new Matrix(components);
+    }
+
+    public Matrix getDifference(Matrix matrix) {
+        if (getColumnCount() != matrix.getColumnCount() || getLineCount() != matrix.getLineCount()) {
+            throw new ArithmeticException(String.format("Разность не определена, размерность матриц должна быть равна, " +
+                    "Матрица 1 - %d x %d, Матрица 2 - %d x %d", getLineCount(), getColumnCount(), matrix.getLineCount(), matrix.getColumnCount()));
+        }
+
+        for (int i = 0; i < components.length; i++) {
+            components[i].subtract(matrix.components[i]);
+        }
+
+        return new Matrix(components);
+    }
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        return new Matrix(matrix1.getSum(matrix2));
+    }
+
+    public static Matrix getDifference(Matrix matrix1, Matrix matrix2) {
+        return new Matrix(matrix1.getDifference(matrix2));
+    }
+
+    public static Matrix getProduct(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getColumnCount() != matrix2.getLineCount()) {
+            throw new ArithmeticException(String.format("Произведение не определено, количество столбцов первой матрицы - %d не равно количеству строк второй матрицы - %d",
+                    matrix1.getColumnCount(), matrix2.getLineCount()));
+        }
+
+        Matrix resultMatrix = new Matrix(matrix1.getLineCount(), matrix2.getColumnCount());
+
+        for (int i = 0; i < resultMatrix.components.length; i++) {
+            for (int j = 0; j < matrix2.getColumnCount(); j++) {
+                resultMatrix.components[i].setComponent(j, Vector.getScalarProduct(matrix1.components[i], matrix2.getColumnVector(j)));
+            }
+        }
+
+        return resultMatrix;
+    }
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder().append("{");
 
         for (Vector component : components) {
-            stringBuilder.append(component).append(", ").append("\n");
+            stringBuilder.append(component).append(", ");
         }
 
-        stringBuilder.delete(stringBuilder.length() - 3, stringBuilder.length()).append("}");
+        stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length()).append("}");
 
         return String.valueOf(stringBuilder);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Matrix matrix = (Matrix) o;
+        return Arrays.equals(components, matrix.components);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(components);
     }
 }
--- Matrix/src/ru/academits/kazantsev/matrix_main/Main.java
+++ Matrix/src/ru/academits/kazantsev/matrix_main/Main.java
@@ -5,36 +5,48 @@ import ru.academits.kazantsev.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        double[][] components = {{1, 2, 3}, {2, 5}, {3}};
+        double[][] components = {{2.0, 7.0}, {3.0, 1.0}, {3.0, 2.0}};
 
-        Vector[] vectors = {
-                new Vector(new double[]{1, 2, 4, 5}),
-                new Vector(new double[]{3, 5, 2, 3, 9}),
-                new Vector(new double[]{1, 1, 2}),
-                new Vector(new double[]{3}),
-                new Vector(new double[]{3, 50, 21, 3, 9, 5, 1, 2, 0}),
-                new Vector(new double[]{3, 5}),
+        Vector[] vectors1 = {
+                new Vector(new double[]{2, 7}),
+                new Vector(new double[]{3, 1}),
+                new Vector(new double[]{3, 2})
         };
 
+        Vector[] vectors2 = {
+                new Vector(new double[]{3.1, 7, 8, -8, 1, 2, 4.5}),
+                new Vector(new double[]{5, 8, 1, 12, 2, -1, -50})
+        };
+
+        Vector[] vectors3 = {
+                new Vector(new double[]{3.1}),
+                new Vector(new double[]{5}),
+                new Vector(new double[]{20}),
+                new Vector(new double[]{-17})
+        };
+
+        Vector vector = new Vector(new double[]{3.1, 1, 9, 22});
         Matrix matrix1 = new Matrix(5, 5);
         Matrix matrix2 = new Matrix(matrix1);
         Matrix matrix3 = new Matrix(components);
-        Matrix matrix4 = new Matrix(vectors);
+        Matrix matrix4 = new Matrix(vectors1);
+        Matrix matrix5 = new Matrix(vectors1);
+        Matrix matrix6 = new Matrix(vectors1);
+        Matrix matrix7 = new Matrix(vectors2);
+        Matrix matrix8 = new Matrix(vectors3);
 
         matrix2.setComponent(3, new Vector(new double[]{1, 2, 3, 4, 5}));
-        System.out.println(matrix2.getComponent(3));
+        System.out.println("Вектор-строка по заданному индексу: " + matrix2.getLineVector(3));
 
-//        System.out.println(matrix1);
-//        System.out.println();
-//        System.out.println(matrix2);
-//        System.out.println();
-        System.out.println(matrix4);
-        System.out.println();
-//        System.out.println(matrix2);
-//        System.out.println();
-//        System.out.println(matrix1);
-//        System.out.println();
-        System.out.println(matrix3);
-
+        System.out.printf("Количество строк матрицы: %d, столбцов: %d%n", matrix3.getLineCount(), matrix3.getColumnCount());
+        System.out.println("Столбец-вектор по указанному индексу: " + matrix4.getColumnVector(0));
+        System.out.println("Исходная матрица, до транспонирования: " + matrix5);
+        System.out.println("Транспонированная матрица: " + matrix5.transpose());
+        System.out.println("Сумма матриц 5 и 3: " + matrix5.getSum(matrix3));
+        System.out.println("Разность матриц 5 и 3: " + matrix5.getDifference(matrix3.getProduct(4)));
+        System.out.println("Произведение матрицы 8 и вектора: " + matrix8.multiplyByVector(vector));
+        System.out.println("Статический метод определения суммы матриц: " + Matrix.getSum(matrix3, matrix5));
+        System.out.println("Статический метод определения разности матриц: " + Matrix.getDifference(matrix5, matrix3));
+        System.out.println("Произведение матриц 6 и 7: " + Matrix.getProduct(matrix6, matrix7));
     }
 }
